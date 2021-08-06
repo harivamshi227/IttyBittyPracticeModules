3bfe2ad5f74f8ef6ba4e8620690c15297bc4b8a4
harivamshi227 <87935976+harivamshi227@users.noreply.github.com>
2021-08-06 20:32:32 +0530

    New Solution Created

---
+++ IttyBittyPracticeModules/IttyBittyPracticeModules/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IttyBittyPracticeModules
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
